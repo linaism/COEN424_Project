576816f66ead87c1555e2ae803aba74ed9f8f73d
Lina Ismail <ilina19@hotmail.com>
2023-11-25 19:53:34 -0500

    made flask app instead

---
+++ app.py
@@ -0,0 +1,17 @@
+import gradio as gr
+import joblib
+import numpy as np
+import pandas as pd
+from flask import Flask, render_template, url_for
+
+from omnixai.data.tabular import Tabular
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello"
+
+if __name__ == '__main__':
+    # Start the Gradio interface
+    app.run()
