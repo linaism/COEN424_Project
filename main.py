0715e2194b30564c3055e0dfbc7877b7ef254910
Lina Ismail <ilina19@hotmail.com>
2023-11-25 20:45:30 -0500

    port

--- main.py
+++ main.py
@@ -23,4 +23,4 @@ app = create_app()
 if __name__ == "__main__":
     #    app = create_app()
     print(" Starting app...")
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=5000)
